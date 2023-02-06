761875963922ada0fd761f44b71a9387e6acdd78
Murat Altun <mraltun@gmail.com>
2023-02-06 16:17:15 +0300

    Global variables in next config

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import persistStore from "redux-persist";
+import { persistStore } from "redux-persist";
 import store from "@/store";
 import "@/styles/globals.scss";
 
--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import persistReducer from "redux-persist";
+import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import cart from "./cartSlice";
