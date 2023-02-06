9bb59be718252343e65e12e53033726769794ad2
Murat Altun <mraltun@gmail.com>
2023-02-06 16:35:03 +0300

    Added Header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
@@ -8,10 +9,18 @@ let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />;
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Full-Stack Shop App with NextJS</title>
+        <meta name='description' content='Shop App' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />;
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
