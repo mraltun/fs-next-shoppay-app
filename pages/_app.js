093819e9304d4c6b2d1af1ad7fb5951bcf77b67d
Murat Altun <mraltun@gmail.com>
2023-02-06 15:59:15 +0300

    Added redux and store

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist";
+import store from "@/store";
 import "@/styles/globals.scss";
 
+let persistor = persistStore(store);
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />;
+      </PersistGate>
+    </Provider>
+  );
 }
---
+++ store/cartSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {},
+});
+
+export default cartSice.reducer;
