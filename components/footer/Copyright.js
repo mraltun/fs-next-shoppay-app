532386ddbbd76eae257f1faabc80e20065c6dffd
Murat Altun <mraltun@gmail.com>
2023-02-13 13:31:37 +0300

    Added Copyright and css

---
+++ components/footer/Copyright.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { IoLocationSharp } from "react-icons/io5";
+
+const data = [
+  {
+    name: "Privacy Center",
+    link: "",
+  },
+  {
+    name: "Privacy & Cookie Policy",
+    link: "",
+  },
+  {
+    name: "Manage Cookies",
+    link: "",
+  },
+  {
+    name: "Terms & Conditions",
+    link: "",
+  },
+  {
+    name: "Copyright Notice",
+    link: "",
+  },
+];
+
+export default function Copyright({ country }) {
+  return (
+    <div className={styles.footer__copyright}>
+      <section>©2023 Shop App. All Rights Resereved.</section>
+      <section>
+        <ul>
+          {data.map((link) => (
+            <li>
+              <Link href={link.link}>{link.name}</Link>
+            </li>
+          ))}
+          <li>
+            <a>
+              <IoLocationSharp /> Turkiye
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
